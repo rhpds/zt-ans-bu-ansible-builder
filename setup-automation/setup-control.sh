76228ad47ac3741ec0b209f0e07dfb0e7163e28c
Alex Walczyk <abwalczyk@gmail.com>
2026-02-27 11:32:36 -0500

    testing

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -2,17 +2,17 @@
 set -e
 
 
-# Enable standard RHEL repos if available
-sudo dnf config-manager --set-enabled rhel-9-baseos-rpms rhel-9-appstream-rpms || true
+# # Enable standard RHEL repos if available
+# sudo dnf config-manager --set-enabled rhel-9-baseos-rpms rhel-9-appstream-rpms || true
 
-# Install EPEL if Satellite not available
-if ! sudo dnf repolist | grep -q epel; then
-  sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm || true
-fi
+# # Install EPEL if Satellite not available
+# if ! sudo dnf repolist | grep -q epel; then
+#   sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm || true
+# fi
 
-# Allow passwordless sudo
-echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
-chmod 440 /etc/sudoers.d/rhel_sudoers
+# # Allow passwordless sudo
+# echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
+# chmod 440 /etc/sudoers.d/rhel_sudoers
 
-# Install Certbot
-sudo dnf install -y certbot jq
+# # Install Certbot
+# sudo dnf install -y certbot jq
